e4b34ae560d597576ee46c68776cda96035636ae
LevitCastro85 <levitcastromarcial@gmail.com>
2025-07-10 22:24:58 -0700

    fix: remove duplicate AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from '@/App';
 import '@/index.css';
 
-// 👇 Importa el AuthProvider
-import { AuthProvider } from '@/contexts/AuthContext';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider> {/* 👈 Rodea la app con el contexto */}
-      <App />
-    </AuthProvider>
+    <App />
   </React.StrictMode>
 );
